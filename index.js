5e78ed8ecc7d41215f258ac9b6c8ad308f00df63
Mojaman <141308466+Mojaman@users.noreply.github.com>
2025-10-18 11:11:38 +0900

    replitからテンプレを入れてみた

---
+++ index.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const http = require("http");
+const socketIo = require("socket.io");
+const path = require("path");
+
+const app = express();
+const server = http.createServer(app);
+const io = socketIo(server);
+
+app.use(express.static("public"));
+
+const PORT = process.env.PORT || 5000;
+server.listen(PORT, "0.0.0.0", () => {
+});
